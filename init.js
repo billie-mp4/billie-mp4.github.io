addbc4a3172b84659500e5ebb057ad9323fb14ca
billie-mp4 <101739643+billie-mp4@users.noreply.github.com>
2025-08-31 22:40:54 +0900

    rename

--- init.js
+++ init.js
@@ -1,12 +1,12 @@
-function initBuffers(gl){
-    const posBuffer=initPositionBuffer(gl)
+function init(gl){
+    const posBuffer=initPos(gl)
     return {position:posBuffer}
 }
-function initPosBuffer(gl){
+function initPos(gl){
     const posBuffer=gl.createBuffer()
     gl.bindBuffer(gl.ARRAY_BUFFER,posBuffer)
     const pos=[1,1,-1,1,1,-1,-1,-1]
     gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(pos),gl.STATIC_DRAW)
     return posBuffer
 }
-export {initBuffers}
+export {init}
